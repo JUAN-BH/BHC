0b05196a6b3b0fbe662230965506ce630f161415
JUAN-BH <juanmita_bh@hotmail.com>
2023-04-27 21:55:49 -0500

    fixing skills routes

--- src/utils/skills.js
+++ src/utils/skills.js
@@ -1,41 +1,41 @@
 const html = {
-    img: '/src/assets/imgSkills/html.svg',
+    img: './src/assets/imgSkills/html.svg',
     name: 'html'
 }
 const css = {
-    img: '/src/assets/imgSkills/css.svg',
+    img: './src/assets/imgSkills/css.svg',
     name: 'css'
 }
 const js = {
-    img: '/src/assets/imgSkills/js.svg',
+    img: './src/assets/imgSkills/js.svg',
     name: 'js'
 }
 const webpack = {
-    img: '/src/assets/imgSkills/webpack.svg',
+    img: './src/assets/imgSkills/webpack.svg',
     name: 'webpack'
 }
 const sass = {
-    img: '/src/assets/imgSkills/sass.svg',
+    img: './src/assets/imgSkills/sass.svg',
     name: 'sass'
 }
 const react = {
-    img: '/src/assets/imgSkills/reactIcon.png',
+    img: './src/assets/imgSkills/reactIcon.png',
     name: 'react'
 }
 const vite = {
-    img: '/src/assets/imgSkills/vite.svg',
+    img: './src/assets/imgSkills/vite.svg',
     name: 'vite'
 }
 const redux = {
-    img: '/src/assets/imgSkills/redux.png',
+    img: './src/assets/imgSkills/redux.png',
     name: 'redux'
 }
 const formik = {
-    img: '/src/assets/imgSkills/formik.png',
+    img: './src/assets/imgSkills/formik.png',
     name: 'formik'
 }
 const tailwind = {
-    img: '/src/assets/imgSkills/tailwind.png',
+    img: './src/assets/imgSkills/tailwind.png',
     name: 'tailwind'
 }
 
